libpixman/pixman

commit 6d2cf40166d81bfc63108504c8022dc4fec37ff5

MIPS: Fix exported symbols in public API.

pixman/pixman-mips-dspr2-asm.h
@@ -72,7 +72,10 @@
 #define LEAF_MIPS32R2(symbol)                           \
                 .globl  symbol;                         \
                 .align  2;                              \
+#ifdef __ELF__
+                .hidden symbol;                         \
                 .type   symbol, @function;              \
+#endif
                 .ent    symbol, 0;                      \
 symbol:         .frame  sp, 0, ra;                      \
                 .set    push;                           \

commit cf086d4949092861dc3729465a3881d229cc1060

MIPS: Drop #ifdef __ELF__ in definition of LEAF_MIPS32R2
Commit 6d2cf40166d8 ("MIPS: Fix exported symbols in public API") attempted to
add a .hidden assembly directive, conditional on the code being compiled for an
ELF target. Unfortunately the #ifdef added was already inside a macro and
wasn't expanded properly by the preprocessor.

Fix by removing the check. It's unlikely there are many non-ELF MIPS systems
around anyway.

Fixes: Bug 83358 (https://bugs.freedesktop.org/83358)
Fixes: 6d2cf40166d8 ("MIPS: Fix exported symbols in public API")
Signed-off-by: James Cowgill <james410@cowgill.org.uk>
Cc: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
Cc: Nemanja Lukic <nemanja.lukic@rt-rk.com>
Acked-by: Siarhei Siamashka <siarhei.siamashka@gmail.com>
Reviewed-by: Pekka Paalanen <pekka.paalanen@collabora.co.uk>

pixman/pixman-mips-dspr2-asm.h
@@ -72,10 +72,8 @@
 #define LEAF_MIPS32R2(symbol)                           \
                 .globl  symbol;                         \
                 .align  2;                              \
-#ifdef __ELF__
                 .hidden symbol;                         \
                 .type   symbol, @function;              \
-#endif
                 .ent    symbol, 0;                      \
 symbol:         .frame  sp, 0, ra;                      \
                 .set    push;                           \